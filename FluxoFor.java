657a6495e55a5bf54173d8194d856b97d71ec6c3
RodrigoCosta1983 <rodrigo25.inf@outlook.com>
2022-04-08 12:50:47 -0300

    Update

---
+++ FluxoFor.java
@@ -0,0 +1,13 @@
+public class FluxoFor {
+
+    public static void main(String[] args) {
+        int tamanho = 5;
+
+        for (int i = 0; i < tamanho; i++) {
+            
+          //  System.out.println("for ");
+            System.out.print("for ");
+            
+        }
+    }
+}
---
+++ FluxoForeach.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+
+public class FluxoForeach {
+    public static void main(String[] args) {
+        int[] pares = {2,4,6,8};
+
+        for (int i=0; i<pares.length; i++) {
+            int par = pares[i];
+
+            System.out.println(par);
+        }
+
+        for (int a : pares) {
+            System.out.println(a);
+        }
+
+        /** ******************* */
+
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for (int i = 0; i < 10; i++) {
+            list.add(i);
+            
+        }
+        for (Integer numero : list) {
+            System.out.print(numero);
+        }
+
+
+         
+
+    }
+}
