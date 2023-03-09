773445beaa6db48bc75d7f4f6883f44dae27aa9f
rodrigocosta1983 <rodrigo25.inf@outlook.com>
2023-03-09 02:25:13 -0300

    Encapsulamento

--- CursoJava/src/javaPoo/PessoaApp.java
+++ CursoJava/src/javaPoo/PessoaApp.java
@@ -4,15 +4,17 @@ public class PessoaApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		Pessoa pessoa1 = new Pessoa(1, "Sophie");
 		Pessoa pessoa2 = new Pessoa(2, "Hellen");
-		Pessoa pessoa3 = new Pessoa(3, "Rod");
-		
-		System.out.println("O meu nome eh: "+pessoa1.getNome());
-		System.out.println("O meu nome eh: "+pessoa2.getNome());
-		System.out.println("O meu nome eh: "+pessoa3.getNome());
 
+		Pessoa codPessoa1 = new Pessoa(1, null);
+
+		System.out.println("O meu nome eh: " + pessoa1.getNome());
+		System.out.println("O meu nome eh: " + pessoa2.getNome());
+		System.out.println("O meu cod eh: " + codPessoa1.getCodigo());
+
+		System.out.println("Cod: " + codPessoa1.getCodigo() + " - Nome: " + pessoa1.getNome());
 	}
 
 }
