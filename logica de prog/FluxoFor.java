2bead03f9329eb908e7e67f8f420255770305b04
rodrigocosta1983 <rodrigo25.inf@outlook.com>
2023-03-21 04:22:44 -0300

    UP For

--- logica de prog/FluxoFor.java
+++ logica de prog/FluxoFor.java
@@ -1,3 +1,5 @@
+import java.util.Iterator;
+
 public class FluxoFor {
 
 	public static void main(String[] args) {
@@ -6,8 +8,16 @@ public class FluxoFor {
 		for (int i = 0; i < tamanho; i++) {
 
 			// System.out.println("for ");
-			System.out.print("for ");
+			//System.out.print("for ");
 
 		}
+		
+		for(int x=0; x<tamanho; x++) {
+			for(int i=0; i<tamanho; i++) {
+				System.out.print("* ");			
+			}
+			System.out.println();
+		}
+		
 	}
 }
