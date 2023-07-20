0f09d32284ce15d9ac053f569c0dc584c5a0a44e
User <User@RODRIGO-SURFACE-02>
2023-07-20 20:12:10 -0300

    ADD Surface

--- CursoJava/src/javaPoo/ClienteApp.java
+++ CursoJava/src/javaPoo/ClienteApp.java
@@ -3,7 +3,7 @@ package javaPoo;
 public class ClienteApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/// TODO Auto-generated method stub
 
 		ClientePF clientepf1 = new ClientePF("xxxxx", "BairroX", "12134");
 		ClientePJ clientepj1 = new ClientePJ("zzzzz", "BairroZ", "5432");
