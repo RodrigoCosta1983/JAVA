8c328469588519507849580d7ebf5c9b0edb07da
User <User@RODRIGO-SURFACE-02>
2023-08-04 00:03:04 -0300

    Uptd

--- CursoJava/src/javaPoo/ClienteApp.java
+++ CursoJava/src/javaPoo/ClienteApp.java
@@ -5,12 +5,13 @@ public class ClienteApp {
 	public static void main(String[] args) {
 		/// TODO Auto-generated method stub
 
-		ClientePF clientepf1 = new ClientePF("xxxxx", "BairroX", "12134");
-		ClientePJ clientepj1 = new ClientePJ("zzzzz", "BairroZ", "5432");
+		ClientePF clientepf1 = new ClientePF("Rodrigo", "Bairro: Xerem", "12134");
+		ClientePJ clientepj1 = new ClientePJ("Sophie", "Bairro: Penha", "5432");
 
 		System.out.println("Cliente PF1 " + clientepf1.getNome());
 		System.out.println("Cliente Pj1 " + clientepj1.getNome());
 		System.out.println("Cliente PF1 " + clientepf1.getNome() + " " + clientepf1.getEnd());
+		System.out.println("Cliente PJ1 " + clientepj1.getNome()+" " + clientepj1.getEnd());
 	}
 
 }
